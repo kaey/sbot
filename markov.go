29d37a6a57688ffc85e7dc10be314f60236f3e0c
Konstantin Kulikov <k.kulikov2@gmail.com>
2016-09-05 02:13:02 +0300

    initial commit

---
+++ markov.go
@@ -0,0 +1,128 @@
+// Copyright 2016 Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// This is copied from https://talks.golang.org/2012/chat.slide
+// and slightly modified for this bot needs.
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math/rand"
+	"strings"
+)
+
+// Prefix is a Markov chain prefix of one or more words.
+type Prefix []string
+
+// String returns the Prefix as a string (for use as a map key).
+func (p Prefix) String() string {
+	return strings.Join(p, " ")
+}
+
+// Shift removes the first word from the Prefix and appends the given word.
+func (p Prefix) Shift(word string) string {
+	elem := p[0]
+	copy(p, p[1:])
+	p[len(p)-1] = word
+	return elem
+}
+
+// LeftShift removes the last word from the Prefix and prepends the given word.
+func (p Prefix) LeftShift(word string) string {
+	elem := p[len(p)-1]
+	copy(p[1:], p[:len(p)-1])
+	p[0] = word
+	return elem
+}
+
+// Chain contains a map ("chain") of prefixes to a list of suffixes.
+// A prefix is a string of prefixLen words joined with spaces.
+// A suffix is a single word. A prefix can have multiple suffixes.
+type Chain struct {
+	prefixChain map[string][]string
+	suffixChain map[string][]string
+	prefixLen   int
+}
+
+// NewChain returns a new Chain with prefixes of prefixLen words.
+func NewChain(prefixLen int) *Chain {
+	return &Chain{make(map[string][]string), make(map[string][]string), prefixLen}
+}
+
+// Build reads text from the provided Reader and
+// parses it into prefixes and suffixes that are stored in Chain.
+func (c *Chain) Build(r io.Reader) {
+	br := bufio.NewReader(r)
+	p := make(Prefix, c.prefixLen)
+	for {
+		var word string
+		if _, err := fmt.Fscan(br, &word); err != nil {
+			break
+		}
+		key := p.String()
+		c.prefixChain[key] = append(c.prefixChain[key], word)
+		prevWord := p.Shift(word)
+		prevKey := p.String()
+		c.suffixChain[prevKey] = append(c.suffixChain[prevKey], prevWord)
+	}
+}
+
+// Generate returns a string of at most n words generated from Chain.
+func (c *Chain) Generate(n int) string {
+	p := make(Prefix, c.prefixLen)
+	var words []string
+	for i := 0; i < n; i++ {
+		choices := c.prefixChain[p.String()]
+		if len(choices) == 0 {
+			break
+		}
+		next := choices[rand.Intn(len(choices))]
+		words = append(words, next)
+		p.Shift(next)
+	}
+	return strings.Join(words, " ")
+}
+
+// GenerateWithKeyword returns a string, generated from Chain, containing keyword.
+func (c *Chain) GenerateWithKeyword(keyword string, n int) string {
+	var p Prefix
+	for prefix := range c.prefixChain {
+		if strings.Contains(prefix, keyword) {
+			p = strings.Split(prefix, " ")
+		}
+	}
+
+	if p == nil {
+		return ""
+	}
+
+	words := make([]string, c.prefixLen)
+	copy(words, p)
+	np := make(Prefix, c.prefixLen)
+	copy(np, p)
+	for i := 0; i < n; i++ {
+		choices := c.prefixChain[np.String()]
+		if len(choices) == 0 {
+			break
+		}
+		next := choices[rand.Intn(len(choices))]
+		words = append(words, next)
+		np.Shift(next)
+	}
+
+	copy(np, p)
+	for i := 0; i < n; i++ {
+		choices := c.suffixChain[np.String()]
+		if len(choices) == 0 {
+			break
+		}
+		prev := choices[rand.Intn(len(choices))]
+		words = append([]string{prev}, words...)
+		np.LeftShift(prev)
+	}
+	return strings.TrimSpace(strings.Join(words, " "))
+}
---
+++ sbot.go
@@ -0,0 +1,210 @@
+// Copyright 2016 Konstantin Kulikov. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Command sbot is a stupid SB bot.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"os"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/BurntSushi/toml"
+	_ "github.com/denisenkom/go-mssqldb"
+	"github.com/jmoiron/sqlx"
+	"github.com/tucnak/telebot"
+)
+
+var (
+	pidPath  = flag.String("pid", "", "Path to pid file. if empty, pid is not written.")
+	logPath  = flag.String("log", "", "Path to log file. If empty, log goes to stderr.")
+	confPath = flag.String("config", "sbot.conf", "Path to config file")
+)
+
+// Config is a program configuration.
+type Config struct {
+	DB     string
+	Token  string
+	ChatID int64
+}
+
+// TTSMessage represent single TTS message.
+type TTSMessage struct {
+	ID           int    `db:"MSGID"`
+	Text         string `db:"MSGText"`
+	Author       string `db:"MSGAuthor"`
+	SectionID    int    `db:"MSGSectionID"`
+	SubsectionID int    `db:"MSGSubSectionID"`
+}
+
+func main() {
+	flag.Parse()
+	rand.Seed(time.Now().UnixNano())
+
+	if err := writePID(*pidPath); err != nil {
+		log.Fatalln(err)
+	}
+
+	if err := writeLog(*logPath); err != nil {
+		log.Fatalln(err)
+	}
+
+	var conf Config
+	if _, err := toml.DecodeFile(*confPath, &conf); err != nil {
+		log.Fatalln(err)
+	}
+
+	ttsdb, err := sqlx.Open("mssql", conf.DB)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer ttsdb.Close()
+
+	c, err := buildSBChain(ttsdb)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var lastProcessedID int
+	if err := ttsdb.Get(&lastProcessedID, "select top 1 MSGID from MSGS order by MSGID DESC"); err != nil {
+		log.Fatalln(err)
+	}
+
+	bot, err := telebot.NewBot(conf.Token)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	messages := make(chan telebot.Message)
+	ticker := time.NewTicker(5 * time.Minute)
+	chat := telebot.Chat{
+		ID:   conf.ChatID,
+		Type: "group",
+	}
+	pollQ := "select MSGID,MSGText,MSGAuthor,MSGSectionID,MSGSubSectionID from MSGS where CreatedBy in (10,109) and MSGSectionID in (3,6,10) and MSGID > $1 order by MSGID ASC"
+
+	bot.Listen(messages, 1*time.Second)
+	log.Println("Serving")
+
+	for {
+		select {
+		case msg := <-messages:
+			if msg.Chat.ID != chat.ID && msg.Sender.Username != "rammaq" {
+				bot.SendMessage(msg.Chat, "denied", nil)
+				continue
+			}
+
+			if len(strings.Split(msg.Text, " ")) != 1 {
+				continue
+			}
+
+			text := c.GenerateWithKeyword(msg.Text, 100)
+			if text == "" {
+				bot.SendMessage(msg.Chat, "Что?", nil)
+				continue
+			}
+			bot.SendMessage(msg.Chat, text, nil)
+		case <-ticker.C:
+			if chat.ID == 0 {
+				continue
+			}
+			var messages []TTSMessage
+			if err := ttsdb.Select(&messages, pollQ, lastProcessedID); err != nil {
+				log.Println(err)
+				continue
+			}
+			if len(messages) == 0 {
+				continue
+			}
+			lastProcessedID = messages[len(messages)-1].ID
+			log.Printf("got %v messages, last ID: %v", len(messages), lastProcessedID)
+			for _, msg := range filterMessages(messages) {
+				bot.SendMessage(
+					chat,
+					fmt.Sprintf("%s:\n%s", msg.Author, msg.Text),
+					nil,
+				)
+			}
+		}
+	}
+}
+
+func buildSBChain(ttsdb *sqlx.DB) (*Chain, error) {
+	q := "select MSGID,MSGText,MSGAuthor,MSGSectionID,MSGSubSectionID from MSGS where CreatedBy in (10) and MSGSectionID in (3,6,10) and MSGDate > $1 order by MSGID ASC"
+	var messages []TTSMessage
+	if err := ttsdb.Select(&messages, q, time.Now().Add(-1*2*365*24*time.Hour)); err != nil {
+		return nil, err
+	}
+
+	c := NewChain(3)
+	for _, msg := range filterMessages(messages) {
+		c.Build(strings.NewReader(msg.Text))
+	}
+
+	return c, nil
+}
+
+func filterMessages(messages []TTSMessage) []TTSMessage {
+	var filteredMessages []TTSMessage
+	for _, msg := range messages {
+		if strings.HasPrefix(msg.Text, "Назначен ответственный") {
+			continue
+		}
+		if strings.HasPrefix(msg.Text, "Назначен исполнитель") {
+			continue
+		}
+		if strings.HasPrefix(msg.Text, "Ответственный ") {
+			continue
+		}
+		if strings.HasPrefix(msg.Text, "Закрытие заявки") {
+			continue
+		}
+		if strings.HasPrefix(msg.Text, "Инцидент") {
+			continue
+		}
+		if strings.HasPrefix(msg.Text, "Заявка") {
+			continue
+		}
+		if strings.HasPrefix(msg.Text, "Клиенту отправлено сообщение:") {
+			continue
+		}
+		if len(strings.Split(msg.Text, " ")) < 5 {
+			continue
+		}
+		filteredMessages = append(filteredMessages, msg)
+	}
+
+	return filteredMessages
+}
+
+func writePID(path string) error {
+	if path == "" {
+		return nil
+	}
+	pid := fmt.Sprintf("%v", os.Getpid())
+	return ioutil.WriteFile(path, []byte(pid), 0644)
+}
+
+func writeLog(path string) error {
+	if path == "" {
+		return nil
+	}
+	os.Stdin.Close()
+	os.Stdout.Close()
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		return err
+	}
+
+	if err := syscall.Dup2(int(f.Fd()), int(os.Stderr.Fd())); err != nil {
+		return err
+	}
+	return nil
+}
